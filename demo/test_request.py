8f3570ecba8b5edc2e4f56aa8dc16d3529c539bf
zijianzhang <342088102@qq.com>
2019-01-17 00:43:55 +0800

    test

--- demo/test_request.py
+++ demo/test_request.py
@@ -1,9 +1,8 @@
 import requests,json,base64
 
 
-url = "http://127.0.0.1:5000/play/nb/2"
+url = "http://127.0.0.1:8080"
 with open('owater.jpg','rb') as p:
     fileBase64 = base64.b64encode(p.read()).decode("utf-8")
-r = requests.post(url,json={'img':fileBase64})
-
+r = requests.get(url)
 print(r.json())
